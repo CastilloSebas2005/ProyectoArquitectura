bb55a00d1c7432020523a2ece0799ad871e04162
Melanny Hernández Rivera <89104563+Melahdezri00@users.noreply.github.com>
2024-06-24 23:45:57 -0600

    Implementando funciones restantes
    Se implementan las siguientes funciones para el juego:
    guardarLugar
    comprobarGanador

--- gatoJuego.asm
+++ gatoJuego.asm
@@ -151,7 +151,7 @@ dibujarHorizontalCentral:
 	li $t9, 128
 	
 dibujarHorizontalInferior: 
-	bge $t0, $t9, casillaAMarcar 
+	bge $t0, $t9, posicionarSelector 
 	jal dibujarPixel
 	addi $t0, $t0, 1
 	addi $a0, $a0, 1
@@ -198,26 +198,29 @@ dibujarX:
 	addi $a0, $a0, -10
 	addi $a1,$a1, -10
 	li $t0, 0
- dibujarBarraIquierda:
+dibujarBarraIzquierda:
  	bge $t0, 20, dibujarBarraDerecha
 	jal dibujarPixel
 	addi $t0, $t0, 1
 	addi $a0, $a0, 1
 	addi $a1, $a1, 1
 	j dibujarBarraIzquierda
+	
 reinicioDeValores: 	
  	addi $a1,$a1, -20
  	li $t0, 0
+ 	
  dibujarBarraDerecha:
- 	bge $t0, 20, Xterminada
+ 	bge $t0, 20, XTerminada
 	jal dibujarPixel
 	addi $t0, $t0, 1
 	addi $a0, $a0, -1
 	addi $a1, $a1, 1
 	j dibujarBarraDerecha
+	
  XTerminada:	
  #luego de dibujar se verifica el ganador como se indicaba en el diagrama de flujo
- 	jal verificarGanador
+ 	jal comprobarGanador
  	
  	li $a0, 64
 	li $a1, 64
@@ -333,9 +336,286 @@ arcoIzquierdoInferior:
 	
 OTerminado:
 # cÃ­rculo terminado, ahora saltar de regreso y verificar si ganÃ³
-	jal verificarVictoria
+	jal comprobarGanador
 	li $a0, 64
 	li $a1, 64
 	li $a2, BLANCO
 	addi $t8, $t8, 1
 	j ciclo
+
+#Guarda donde se colocan los elementos en el tablero.
+guardarLugar:
+#Comportamiento del tablero desde s0 hasta s7     
+#Se guarda el color y la ubicacion de la figura (ROJO = x    AZUL = O)
+
+	addi $sp, $sp, -4
+	sw $ra, ($sp)
+	
+		#
+		s0:
+			bge $a0, 43, s1 #ciclo s1 
+			bge $a1, 43, s1
+			
+				beq $a2, ROJO, s0r
+				beq $a2, AZUL, s0b
+				
+				s0r: 
+				li $s0, 1
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+				s0b: 
+				li $s0, 2
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+		
+		#						
+		s1:
+			bge $a0, 86, s2 #ciclo s2 
+			bge $a1, 43, s2
+			beq $a2, ROJO, s1r
+				beq $a2, AZUL, s1b
+				s1r: 
+				li $s1, 1
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+				s1b: 
+				li $s1, 2
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+		
+		#		
+		s2:
+			bge $a0, 127, s3 #ciclo s3
+			bge $a1, 43, s3
+			beq $a2, ROJO, s2r
+				beq $a2, AZUL, s2b
+				s2r: 
+				li $s2, 1
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+				s2b: 
+				li $s2, 2
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+		
+		#				
+		s3:
+			bge $a0, 43, s4 #ciclo s4 
+			bge $a1,86, s4
+			beq $a2, ROJO, s3r
+				beq $a2, AZUL, s3b
+				s3r: 
+				li $s3, 1
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+				s3b: 
+				li $s3, 2
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+		
+		#				
+		s4:
+			bge $a0, 86, s5 #ciclo s5 
+			bge $a1, 86, s5
+			beq $a2, ROJO, s4r
+				beq $a2, AZUL, s4b
+				s4r: 
+				li $s4, 1
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+				s4b: 
+				li $s4, 2
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+			
+		#			
+		s5:
+			bge $a0, 128, s6 #ciclo s6 
+			bge $a1, 86, s6
+			beq $a2, ROJO, s5r
+				beq $a2, AZUL, s5b
+				s5r: 
+				li $s5, 1
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+				s5b: 
+				li $s5, 2
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+				
+		#		
+		s6:
+			bge $a0, 43, s7 #ciclo s7
+			bge $a1, 128, s7
+			beq $a2, ROJO, s6r
+				beq $a2, AZUL, s6b
+				s6r: 
+				li $s6, 1
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+				s6b: 
+				li $s6, 2
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+				
+		#		
+		s7:	
+			bge $a0, 86, t6 
+			bge $a1, 128, t6
+			beq $a2, ROJO, s7r
+				beq $a2, AZUL, s7b
+				s7r: 
+				li $s7, 1
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+				s7b: 
+				li $s7, 2
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+				
+		#t6 guarda 1 si es x & 2 si es O 
+		t6:
+			beq $a2, ROJO, t6r
+			beq $a2, AZUL, t6b
+				t6r: 
+				li $t6, 1
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+				t6b: 
+				li $t6, 2
+				lw $ra, ($sp)
+				addi $sp, $sp, 4
+				jr $ra
+				
+						
+#Se verifica cada cuadrícula del tablero para comprobar si hay ganador
+#Si hay ganador, se dibuja una línea sobre las tres casillas ganadoras								
+comprobarGanador:
+	addi $sp, $sp, -4
+	sw $ra, ($sp)
+	li $t0, 0
+	li $a2, AMARILLO #la linea que se dibuja sobre las sillas es amarilla
+	
+	vertical1:
+		beq $s0, $s3, vertical1con2
+		j vertical2
+		vertical1con2:
+		li $a0, 22
+		li $a1, 0
+		beq $s3, $s6, dibujarVertical
+		
+	vertical2:
+		beq $s1, $s4, vertical2con2
+		j vertical3
+		vertical2con2:
+		li $a0, 64
+		li $a1, 0
+		beq $s4, $s7, dibujarVertical
+		
+	vertical3:
+		beq $s2, $s5, vertical3con2
+		j horizontal1
+		vertical3con2:
+		li $a0, 107
+		li $a1, 0
+		beq $s5 $t6, dibujarVertical
+		
+	horizontal1:
+		beq $s0, $s1, horizontal1con2
+		j horizontal2
+		horizontal1con2:
+			li $a0, 0
+			li $a1, 22
+			beq $s1, $s2, dibujarHorizontal
+			
+	horizontal2:
+		beq $s3, $s4, horizontal2con2
+		j horizontal3
+		horizontal2con2:
+			li $a0, 0
+			li $a1, 64
+			beq $s4, $s5, dibujarHorizontal
+			
+	horizontal3:
+		beq $s6, $s7 horizontal3con2
+		j diagonalDerecha
+		horizontal3con2:
+			li $a0, 0
+			li $a1, 108
+			beq $s7, $t6, dibujarHorizontal
+			
+	diagonalDerecha:
+		beq $s2, $s4, diagonalDerechacon2
+		j diagonalIzquierda
+		diagonalDerechacon2:
+			li $a0, 127
+			li $a1, 0
+			li $t0, 0
+			beq $s4, $s6, dibujarDiagonalDerecha
+			
+	diagonalIzquierda:
+		beq $s0, $s4, diagonalIzquierdacon2
+		j backj
+		diagonalIzquierdacon2:
+			li $a0, 0
+			li $a1, 0
+			beq $s4, $t6, dibujarDiagonalIzquierda
+			j backj
+			
+#Se dibuja la línea ganadora en vertical							
+ dibujarVertical:
+ 	
+ 	beq $t0, 128, backj
+ 	jal dibujarPixel
+ 	addi $t0, $t0, 1
+ 	addi $a1, $a1, 1
+ 	j dibujarVertical
+ 	
+#Se dibuja la línea ganadora en horizontal	
+ dibujarHorizontal:
+ 	beq $t0, 128, backj
+ 	jal dibujarPixel
+ 	addi $t0, $t0, 1
+ 	addi $a0, $a0, 1
+ 	j dibujarHorizontal
+ 	
+ #Se dibuja la línea diagonal izquierda ganadora
+ dibujarDiagonalIzquierda: 
+ 	beq $t0, 512, backj
+ 	jal dibujarPixel
+ 	addi $t0, $t0, 1
+ 	addi $a0, $a0, 1
+ 	addi $a1, $a1, 1
+ 	j dibujarDiagonalIzquierda
+ 	
+ #Se dibuja la línea diagonal derecha ganadora	
+ dibujarDiagonalDerecha: 
+ 	beq $t0, 512, backj
+ 	jal dibujarPixel
+ 	addi $t0, $t0, 1
+ 	addi $a0, $a0, -1
+ 	addi $a1, $a1, 1
+ 	j dibujarDiagonalDerecha
+ 	
+#Arregla el puntero de la pila y salta hacia atrás
+backj:
+	lw $ra, ($sp)
+	addi $sp, $sp, 4
+	jr $ra			
